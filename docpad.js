89cbb09ff91c82357dc6c436545193ee0dab8984
arsley <blue20will@gmail.com>
2019-02-14 19:58:04 +0900

    Initial commit

---
+++ docpad.js
@@ -0,0 +1,13 @@
+// DocPad Configuration File
+// http://docpad.org/docs/config
+
+// Define the DocPad Configuration
+const docpadConfig = {
+  documentPaths: [
+    'pages'
+  ],
+  outPath: 'dist'
+}
+
+// Export the DocPad Configuration
+module.exports = docpadConfig
